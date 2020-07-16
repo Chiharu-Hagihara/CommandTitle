0ca6966b6d10471280f0d37e80b4dd7df7acf68a
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-07-16 14:39:53 +0900

    Java -> Kotlin

--- src/main/java/mc/mec/commandtitle/CommandTitle.kt
+++ src/main/java/mc/mec/commandtitle/CommandTitle.kt
@@ -1,17 +1,13 @@
-package mc.mec.commandtitle;
+package mc.mec.commandtitle
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class CommandTitle extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
+class CommandTitle : JavaPlugin() {
+    override fun onEnable() {
         // Plugin startup logic
-
     }
 
-    @Override
-    public void onDisable() {
+    override fun onDisable() {
         // Plugin shutdown logic
     }
 }
